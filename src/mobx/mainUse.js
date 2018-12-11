dbd326249f3478a19365f8c282c47fe94c5b22b1
suxue <1683248243@qq.com>
2018-12-11 15:57:48 +0800

    edit README, add more examples

--- src/mobx/mainUse.js
+++ src/mobx/mainUse.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
-import {observer,inject} from 'mobx-react';
+import { observer, inject } from 'mobx-react';
 //先注意主入口index.js的store注入
 @inject('timer')
 @observer
 class TimerView extends React.Component {
-    componentDidMount(){
-        setInterval(()=>{
+    componentDidMount() {
+        setInterval(() => {
             this.props.timer.settime()
-        },1000)
+        }, 1000)
     }
     render() {
         return (
-            <span>像redux一样注入: {this.props.timer.time}</span>
+            <>
+                <span>像redux一样注入: {this.props.timer.time}</span>
+                <span>@computed计算属性：{this.props.timer.computedTime}</span>
+            </>
         );
     }
 };
--- src/stores/store.js
+++ src/stores/store.js
@@ -5,5 +5,8 @@ class Time{
     settime(){
         this.time++;
     }
+    @computed get computedTime(){
+        return this.time+1
+    }
 }
 export default new Time()
